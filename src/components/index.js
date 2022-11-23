2e2b85744d87dbf24ba0966ad8d9dcd3a8b97b85
livvyvi3 <machinguravongai@gmail.com>
2022-11-23 06:13:58 +0200

    added a new project to display a modal

--- src/components/index.js
+++ src/components/index.js
@@ -1,44 +1,18 @@
+// Get the modal
+var modal = document.getElementById("myModal");
 
-//make use of a json API to imporve the code
+// Get the button that opens the modal
+var btn = document.querySelector(".btn");
 
-const btn = document.getElementById('changeQuote');
-let quote = document.querySelector('.quote');
-let author = document.querySelector('.author');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-const data = [
-    {
-      "quote": "Sometimes the idea behind a program is one small creative effort.",
-      "Author": "Dan Bricklin"
-    },
-    {
-      "quote": "As a rule, software systems do not work well until they have been used, and have failed repeatedly, in real applications.",
-      "Author": "Dave Parnas"
-    },
-    {
-      "quote": "Complexity has nothing to do with intelligence, simplicity does.",
-      "Author": "Larry Bossidy"
-    },
-    {
-      "quote": "The task of the software development team is to engineer the illusion of simplicity.",
-      "Author": "Grady Booch"
-    },
-    {
-      "quote": "Any optimization that is not about the bottleneck is an illusion of improvement.",
-      "Author": "Federico Toledo"
-    },
-    {
-      "quote": "A good programmer is someone who always looks both ways before crossing a one-way street.",
-      "Author": "Doug Linder"
-    }
-  ];
-  
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-
-btn.addEventListener('click', function handleClick(){
-
-    let random = Math.floor(Math.random() * data.length);
-
-    quote.innerText = data[random].quote;
-    author.innerText = data[random].Author;
-
-})
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
